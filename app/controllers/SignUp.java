426bf3f28ab12da343d7fc5824f5d9baccdd0296
bethdellea <bethdellea@gmail.com>
2016-03-24 11:24:21 -0400

    addUser() is fully functional, routes adjusted to pass it the proper parameters

--- app/controllers/SignUp.java
+++ app/controllers/SignUp.java
@@ -16,6 +16,9 @@ public class SignUp extends Controller{
     }
     public static Result addUser(String emailIn, String passwordIn, String nameIn, String isModVal){
         //this stuff is not being parsed properly. it's also going into the db wrong, probably because of that
+        //System.out.println("Username :" + nameIn + "\t email : " + emailIn + "password : " + passwordIn + "mod status : " + isModVal);
+        //sorry Toby, the println is the most convenient way to do things
+        //also TODO: hide passwords (hashing/encrypting/whatever)
         User newUser = new User();
         newUser.userName = nameIn;
         newUser.email = emailIn;
