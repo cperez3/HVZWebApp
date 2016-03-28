f7f31d9917556da5e72007df281a3a72335cf01a
Mariah <mflaim1@ithaca.edu>
2016-03-28 10:55:12 -0400

    pages for game settings if moderator is or is not in game created

--- app/controllers/ModPage.java
+++ app/controllers/ModPage.java
@@ -1,19 +1,16 @@
 package controllers;
 import play.mvc.Controller;
-import views.html.modPage;
+import views.html.inGameModSettings;
+import views.html.noGameModSettings;
 import play.mvc.Result;
 /**
  * Created by mariahflaim on 3/28/16.
  */
 public class ModPage extends Controller{
     public static Result loadPage(){
-        return ok(modPage.render());
+        //TO DO: check if moderator is in a game or not then load page based off if they are
+        return ok(noGameModSettings.render());
     }
-    public Result showGameSettings(){
-        return ok();
-    }
-    public Result showAddModPage(){
-        return ok();
-    }
+
 }
 
