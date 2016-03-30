486c85436ed5c705292e3defac30be4e30255ab0
bethdellea <bethdellea@gmail.com>
2016-03-30 09:12:57 -0400

    adjusted routes spacing and removed excess comments

--- app/controllers/LogIn.java
+++ app/controllers/LogIn.java
@@ -88,27 +88,3 @@ instead of this one that you may want just logged */ }
     }
 
 }
-
-//I don't know if this down here came from git or was here. sorry.
-/*public class ErrorHandler extends DefaultHttpErrorHandler {
-
-    @Inject
-    public ErrorHandler(Configuration configuration, Environment environment,
-                        OptionalSourceMapper sourceMapper, Provider<Router> routes) {
-        super(configuration, environment, sourceMapper, routes);
-    }
-
-    protected CompletionStage<Result> onProdServerError(RequestHeader request, UsefulException exception) {
-        return CompletableFuture.completedFuture(
-                Results.internalServerError("A server error occurred: " + exception.getMessage())
-        );
-    }
-
-    protected CompletionStage<Result> onForbidden(RequestHeader request, String message) {
-        return CompletableFuture.completedFuture(
-                Results.forbidden("You're not allowed to access this resource.")
-        );
-    }
-}*/
-
-
