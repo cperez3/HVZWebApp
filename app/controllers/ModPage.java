c83f4f2ffd49ed859fcb5334e268f7ed05aa1ed7
Mariah <mflaim1@ithaca.edu>
2016-04-23 14:08:12 -0400

    add Moderator to game is written but not fully tested

--- app/controllers/ModPage.java
+++ app/controllers/ModPage.java
@@ -12,6 +12,7 @@ package controllers;
 import play.db.DB;
 import play.mvc.Controller;
 import play.mvc.Result;
+import scala.Int;
 import views.html.*;
 
 import java.sql.ResultSet;
@@ -49,69 +50,88 @@ public class ModPage extends Controller {
      * creates a new game in the Game class database
      *
      * @param - none
-     * @return - Result redirect to the in Game Mod Setting page
+     * @return - Result redirect to the in Game Mod Setting page or login if not logged in as moderator not in game
      */
     public static Result createGame() {
         //Game game = Form.form(Game.class).bindFromRequest().get();
         // game.save();
         //TO DO : create game and add moderator to it
-        return redirect(routes.ModPage.loadPage());
+        if((session("is_mod")=="true"||session("is_mod")=="1")&&(session("gCode")==null||session("gCode")==" ")){
+            return redirect(routes.ModPage.loadPage());
+        }else{
+            return forbidden(login.render());
+        }
+
     }
 
     /**
      * removes game from database
      *
      * @param - none
-     * @return - Result redirect to the no Game Mod Setting page
+     * @return - Result redirect to the no Game Mod Setting page or login if not logged in as mod in game
      */
     public static Result endGame() {
-        return ok();
+        if((session("is_mod")=="true"||session("is_mod")=="1")&&(session("gCode")!=null||session("gCode")!=" ")) {
+            return loadPage();
+        }else{
+            return forbidden(login.render());
+        }
     }
 
     /**
      * gets rid of moderator status of user and reloads page so that game page is not longer mod page
      *
      * @param - none
-     * @return - none
+     * @return - game page
      */
     public static Result changeStatus() {
-        String isMod = "false";
-        String sql2 = "UPDATE user SET is_mod = " + isMod + " WHERE id = " + session("id");
-        java.sql.Connection conn2 = DB.getConnection();
-        try {
-            //http://stackoverflow.com/questions/18546223/play-framework-execute-raw-sql-at-start-of-request
-
-            java.sql.Statement stmt = conn2.createStatement();
+        if(session("is_mod")=="true"||session("is_mod")=="1") {
+            String isMod = "false";
+            String sql2 = "UPDATE user SET is_mod = " + isMod + " WHERE id = " + session("id");
+            java.sql.Connection conn2 = DB.getConnection();
             try {
-                Boolean rst = stmt.execute(sql2);
-                // rst.close();
-            } finally {
+                //http://stackoverflow.com/questions/18546223/play-framework-execute-raw-sql-at-start-of-request
 
-                stmt.close();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                conn2.close();
+                java.sql.Statement stmt = conn2.createStatement();
+                try {
+                    Boolean rst = stmt.execute(sql2);
+                    // rst.close();
+                } finally {
+
+                    stmt.close();
+                }
             } catch (SQLException e) {
                 e.printStackTrace();
+            } finally {
+                try {
+                    conn2.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
+            //TO DO: Error checking
+            session("is_mod", "false");
+            return loadPage();
+        }else{
+            return forbidden(login.render());
         }
-        //TO DO: Error checking
-        session("is_mod", "false");
-        return ok(regularSettings.render());
     }
 
+    /**
+     * gets all the players that are in the moderators game from the database
+     *
+     * @param - none
+     * @return - render the view players page with the hashmap of players or login page if player  is not a mod in a game
+     */
     public static Result getPlayers() {
-       // String gCode = session("gCode");
-       // if (gCode != null) {
+
+        if((session("is_mod")=="true"||session("is_mod")=="1")&&(session("gCode")!=null||session("gCode")!=" ")){
         Map<String, String>players=new HashMap<String, String>();
         List<String> names=new LinkedList<String>();
         List<String> teams=new LinkedList<String>();
-        session("gCode","1235");
 
-            String sql2 = "SELECT * FROM user WHERE game_code = " + 12345;
+
+            String sql2 = "SELECT * FROM user WHERE game_code = " + Integer.parseInt(session("gCode"));
             java.sql.Connection conn2 = DB.getConnection();
             try {
                 //http://stackoverflow.com/questions/18546223/play-framework-execute-raw-sql-at-start-of-request
@@ -152,11 +172,94 @@ instead of this one that you may want just logged */ }
             }
             //TO DO: this is where you will call the player list page with the players hash map as input
             return ok();
-        }/* else {
+        } else {
             return forbidden(login.render());
-        }*/
+        }
 
 
+    }
+    /**
+     * Adds mod status to a particular user with a particular email
+     *
+     * @param - email of user you want to be moderator
+     * @return - render log in if not logged in as mod in name, reloads page with success message
+     */
+    public static Result addModerator(String email) {
+        if((session("is_mod")=="true"||session("is_mod")=="1")&&(session("gCode")!=null||session("gCode")!=" ")){
+        //String email = "mflaim1@ithaca.edu";
+        String id = "none";
+        String sql2 = "SELECT * FROM user WHERE email = '" + email + "' AND game_code = " + Integer.parseInt(session("gCode"));
+        java.sql.Connection conn2 = DB.getConnection();
+        try {
+            //http://stackoverflow.com/questions/18546223/play-framework-execute-raw-sql-at-start-of-request
+
+            java.sql.Statement stmt = conn2.createStatement();
+            try {
+                ResultSet rst = stmt.executeQuery(sql2);
+                try {
+                    while (rst.next()) {
+
+                        int numColumns = rst.getMetaData().getColumnCount();
+                        id = rst.getString(1);
+
+                    }
+
+                } finally {
+                    try {
+                        rst.close();
+                    } catch (Throwable ignore) { /* Propagate the original exception
+instead of this one that you may want just logged */
+                    }
+                }
+
+                // rst.close();
+            } finally {
+
+                stmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn2.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+            if (id != "none") {
+                String sql3 = "UPDATE user SET is_mod = " + 1 + " WHERE id = " + Integer.parseInt(id);
+                java.sql.Connection conn3 = DB.getConnection();
+                try {
+                    //http://stackoverflow.com/questions/18546223/play-framework-execute-raw-sql-at-start-of-request
+
+                    java.sql.Statement stmt = conn3.createStatement();
+                    try {
+                        Boolean rst = stmt.execute(sql3);
+                        // rst.close();
+                    } finally {
+
+                        stmt.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        conn3.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+                //TO DO: reload page I guess with message of success and if user is not found the load page with fail message
+                return ok();
+            }else{
+                return forbidden();
+            }
+
+        }else{
+            return forbidden(login.render());
+        }
+
+    }
 }
 
 
